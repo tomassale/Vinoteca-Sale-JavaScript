9db0e62b164cc763e669206ebf320069cf950183
tomassale <tomassale@hotmail.com>
2022-04-15 01:21:07 -0300

    Tarea clase 4

--- app.js
+++ app.js
@@ -1,13 +1,76 @@
-let mesas = Number(prompt("Cuantas mesas queres alquilar?"))
-
-for(let i = 1; i <= mesas; i++ ){
-    if (i == 14){
-        console.log("la mesa 14 ya esta reservada")
-    }else if(i == 19){
-        console.log("la mesa 19 ya esta reservada")
-    }else if(i > 80){
-        console.log("no tenemos tantas mesas chinchulin")
-    }else{
-        console.log(`Estan disponibles las mesas ${i}`)
+const pregunta = () =>{
+    let preguntar = prompt("¿Que estas buscando?")
+    switch(preguntar){
+        case "productos":
+            mostrarLista();
+            break
+        case "proveedores":
+            console.log(mostrarProveedor);
+            break
+        case "clientes":
+            console.log(mostrarClientes);
+            break
+        default:
+            alert("No se encuentra lo que busca")
+            break
     }
 }
+const mostrarLista = () => {
+    let buscarLista = prompt("¿Que lista desea buscar?");
+    switch(buscarLista){
+        case "vinos tintos":
+            console.log(vinosTintos);
+            ordenarT();
+            break
+        case "vinos espumosos":
+            console.log(vinosEspumosos);
+            ordenarE();
+            break
+        case "vinos blancos":
+            console.log(vinosBlancos);
+            ordenarB();
+            break
+        default:
+            alert("No se escuentra la lista que busca");
+    }
+}
+const ordenarB = () =>{
+    vinosBlancos.sort((a,b) => {
+        if (a.Productos > b.precio){
+            return 1
+        }
+        else if (a.precio < b.precio) {
+            return -1
+        }
+        else{
+            return 0
+        }
+    })
+}
+const ordenarE = () =>{
+    vinosEspumosos.sort((a,b) => {
+        if (a.Productos > b.precio){
+            return 1
+        }
+        else if (a.precio < b.precio) {
+            return -1
+        }
+        else{
+            return 0
+        }
+    })
+}
+const ordenarT = () =>{
+    vinosTintos.sort((a,b) => {
+        if (a.Productos > b.precio){
+            return 1
+        }
+        else if (a.precio < b.precio) {
+            return -1
+        }
+        else{
+            return 0
+        }
+    })
+}
+pregunta()
