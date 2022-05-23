13ead8a8e92c7e31b86caf7de9b4d833639fbd33
tomassale <tomassale@hotmail.com>
2022-05-23 18:19:56 -0300

    Tarea clase 13

--- app.js
+++ app.js
@@ -30,13 +30,7 @@ let vinosEspumosos = []
 let vinosTintos = []
 
 //pushear clientes
-clientes.push(new Clientes("Nahuel", "Zambito", "frecuente", "Vinos Tintos"))
-clientes.push(new Clientes("Julian", "Rodriguez", "moderado", "Vinos Espumosos"))
-clientes.push(new Clientes("Paula", "Birocco", "nula", "Vinos Blancos"))
-clientes.push(new Clientes("Malena", "Gomez", "nula", "Vinos Espumosos"))
-clientes.push(new Clientes("Carlos", "Juarez", "frecuente", "Vinos Blancos"))
-clientes.push(new Clientes("Alejo", "Fernandez", "regular", "Vinos Tintos"))
-clientes.push(new Clientes("Tomás", "Sale", "moderado", "Vinos Espumosos"))
+
 //pushear proveedores
 proveedores.push(new Proveedores("Salamanca S.A.", "Lanus", "A"))
 proveedores.push(new Proveedores("Fremua S.A.", "La paz", "C"))
@@ -55,6 +49,21 @@ vinosTintos.push(new Productos("Norton Rosado Malbec", 1600, 50))
 vinosTintos.push(new Productos("Dada 7 Finca Las Moras", 1700, 40))
 vinosTintos.push(new Productos("Lambrusco Rosso Amabile", 1900, 54))
 vinosTintos.push(new Productos("Don Valentin", 4000, 26))
+
+//funcion Toastify
+const toastify = (mensaje) => {
+    Toastify({
+        text: mensaje,
+        duration: 2000,
+        close: true,
+        gravity: "bottom",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+    }).showToast();
+}
 
 //creador de array en localStorage
 if(localStorage.getItem('Clientes')){
@@ -83,20 +92,22 @@ if(localStorage.getItem('Vinos Tintos')){
     localStorage.setItem("Vinos Tintos", JSON.stringify(vinosTintos))
 }
 
+
 //Agregar valores a la tabla de clientes
 document.querySelector("#formularioClientes").addEventListener("submit", (e) => {
     e.preventDefault()
     document.querySelector("#tablaClientes").innerHTML += `
-    <tr>
-        <td>${clientesN.value}</td>
-        <td>${clientesA.value}</td>
-        <td>${clientesF.value}</td>
-        <td>${clientesP.value}</td>
-    </tr>
-    `
+        <tr>
+            <td>${clientesN.value}</td>
+            <td>${clientesA.value}</td>
+            <td>${clientesF.value}</td>
+            <td>${clientesP.value}</td>
+        </tr>
+        `
     clientes.push(new Clientes(clientesN.value, clientesA.value, clientesF.value, clientesP.value))
     localStorage.setItem("Clientes", JSON.stringify(clientes))
     document.querySelector("#formularioClientes").reset()
+    toastify("Cliente agregado")
 })
 //Agregar valores a la tabla de proveedores
 document.querySelector("#formularioProveedores").addEventListener("submit", (e) => {
@@ -111,6 +122,7 @@ document.querySelector("#formularioProveedores").addEventListener("submit", (e) 
     proveedores.push(new Proveedores(proveedoresN.value, proveedoresU.value, proveedoresT.value))
     localStorage.setItem("Proveedores", JSON.stringify(proveedores))
     document.querySelector("#formularioProveedores").reset()
+    toastify("Proveedor agregado")
 })
 //Agregar valores a la tabla de vinos blancos
 document.querySelector("#formularioProductosB").addEventListener("submit", (e) => {
@@ -125,6 +137,7 @@ document.querySelector("#formularioProductosB").addEventListener("submit", (e) =
     vinosBlancos.push(new Productos(productosNB.value, productosPB.value, productosSB.value))
     localStorage.setItem("Vinos Blancos", JSON.stringify(vinosBlancos))
     document.querySelector("#formularioProductosB").reset()
+    toastify("Producto agregado")
 })
 //Agregar valores a la tabla de vinos espumosos
 document.querySelector("#formularioProductosE").addEventListener("submit", (e) => {
@@ -138,6 +151,7 @@ document.querySelector("#formularioProductosE").addEventListener("submit", (e) =
     `
     localStorage.setItem("Vinos Espumosos", JSON.stringify(vinosEspumosos))
     document.querySelector("#formularioProductosE").reset()
+    toastify("Producto agregado")
 })
 //Agregar valores a la tabla de vinos tintos
 document.querySelector("#formularioProductosT").addEventListener("submit", (e) => {
@@ -151,6 +165,7 @@ document.querySelector("#formularioProductosT").addEventListener("submit", (e) =
     `
     localStorage.setItem("Vinos Tintos", JSON.stringify(vinosTintos));
     document.querySelector("#formularioProductosT").reset()
+    toastify("Producto agregado")
 })
 
 //Agregar valores del localStorage a la tabla de clientes
